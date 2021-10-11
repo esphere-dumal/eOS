fe57056973274766924775cd88becf4cdc8f4833
esphere-dumal <esphere@qq.com>
2021-10-11 21:51:41 +0800

    :taka:able to load kernel

--- start.sh
+++ start.sh
@@ -4,4 +4,8 @@ nasm -I boot -o bin/loader.bin boot/loader.S
 dd if=./bin/mbr.bin of=./bochs/hd60M.img bs=512 count=1 conv=notrunc
 dd if=./bin/loader.bin of=./bochs/hd60M.img bs=512 count=4 seek=2 conv=notrunc
 
+gcc -c -o ./kernel/main.o ./kernel/main.c
+ld ./kernel/main.o -Ttext 0xc0001500 -e main -o ./kernel/kernel.bin
+dd if=./kernel/kernel.bin of=./bochs/hd60M.img bs=512 count=200 seek=9 conv=notrunc
+
 bochs -f bochs/bochs.disk
