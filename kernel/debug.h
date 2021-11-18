22359bcd2ea5c2cbf7c25e6cd85c62227efa6637
esphere-dumal <esphere@qq.com>
2021-11-18 21:05:10 +0800

    :sparkle: panic_spin

---
+++ kernel/debug.h
@@ -0,0 +1,21 @@
+#ifndef __KERNEL_DEBUG_H
+#define __KERNEL_DEBUG_H
+
+void panic_spin(char *filename, int line, const char *func, const char *condition);
+
+#define PANIC(...) panic_spin(__FILE__, __LINE__, __func__, __VA_ARGS__);
+
+#ifndef NDEBUG
+#define ASSERT(CONDITION) ((void)0)
+
+#else 
+#define ASSERT(CONDITION) \
+    if (CONDITION) {}     \
+    else {
+        PANIC(#CONDITION);\
+    }
+
+#endif  // NDEBUG
+
+
+#endif // __KERNEL_DEBUG_H
